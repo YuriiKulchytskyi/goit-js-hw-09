7083a23698fa4280a7a39af0162040ae0f9d1588
YuriiKulchytskyi <130589503+YuriiKulchytskyi@users.noreply.github.com>
2023-08-25 18:50:03 +0000

    Deploying to gh-pages from @ YuriiKulchytskyi/goit-js-hw-09@dd83b696caf1ad8d0263668634375399216a8d43 🚀

---
+++ 01-color-switcher.3dd54454.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let o=null;t.addEventListener("click",(function(){o=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.3dd54454.js.map
