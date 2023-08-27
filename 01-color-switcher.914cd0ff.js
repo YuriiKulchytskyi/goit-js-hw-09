81a1619393fe5dd42eb0e5b28e1e22f44be9701e
YuriiKulchytskyi <130589503+YuriiKulchytskyi@users.noreply.github.com>
2023-08-27 16:03:06 +0000

    Deploying to gh-pages from @ YuriiKulchytskyi/goit-js-hw-09@5d47d2c982c4eb8a23f6c19a522d4d196a5831be 🚀

---
+++ 01-color-switcher.914cd0ff.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let d=null;e.setAttribute("disabled",!0);t.addEventListener("click",(function(){d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled",!0)})),e.addEventListener("click",(()=>{clearInterval(d),t.removeAttribute("disabled",!0),e.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.914cd0ff.js.map
